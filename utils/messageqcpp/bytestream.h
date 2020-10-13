7fb8741b29b4d5d62e244f2b5e487663fced74f6
Roman Nozdrin <roman.nozdrin@mariadb.com>
2020-10-13 12:43:16 +0000

    MCOL-4313 Very fragile but high speed approach with inline ASM

--- utils/messageqcpp/bytestream.h
+++ utils/messageqcpp/bytestream.h
@@ -151,6 +151,9 @@ public:
      *  push an int128_t onto the end of the stream. The byte order is whatever the native byte order is.
      */
     EXPORT ByteStream& operator<<(const int128_t& o);
+
+    void assign128bitU(const uint128_t& o);
+    void assign128bit(const int128_t& o);
     /**
      *	push an uint128_t onto the end of the stream. The byte order is whatever the native byte order is.
      */
